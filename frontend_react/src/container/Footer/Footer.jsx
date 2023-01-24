b336da5a3198af26dc83215b3670038e595ceca3
Alvin Kariuki <alvinkariuki230@gmail.com>
2023-01-24 12:39:07 +0300

    React frontend

---
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Footer.sass";
+
+const Footer = () => {
+  return <h1>Footer</h1>;
+};
+
+export default Footer;
---
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import "./Skills.sass";
+const Skills = () => {
+  return <h1>Skills</h1>;
+};
+
+export default Skills;
