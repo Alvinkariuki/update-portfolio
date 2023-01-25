f5a2b366e3173382b961d238d418967f8299e911
Alvin Kariuki <alvinkariuki230@gmail.com>
2023-01-25 16:40:07 +0300

    Header section

--- frontend_react/src/constants/images.jsx
+++ frontend_react/src/constants/images.jsx
@@ -34,6 +34,8 @@ import bolt from "../assets/bolt.png";
 import nb from "../assets/nb.png";
 import skype from "../assets/skype.png";
 import spotify from "../assets/spotify.png";
+import meProfile from "../assets/newimg.png";
+import meSmile from "../assets/me-smile.jpg";
 
 export default {
   email,
@@ -69,4 +71,6 @@ export default {
   nb,
   skype,
   spotify,
+  meProfile,
+  meSmile,
 };
