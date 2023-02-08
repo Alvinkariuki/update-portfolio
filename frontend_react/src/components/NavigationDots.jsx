3a7929183cf3a5a4384a993d7d8ea256b61649cc
Alvin Kariuki <alvinkariuki230@gmail.com>
2023-02-08 13:21:33 +0300

    rearranged order

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NavigationDots = ({ active }) => {
   return (
     <div className="app__navigation">
-      {["home", "about", "work", "contact", "testimonials", "skills"].map(
+      {["home", "about", "work", "skills", "contact", "testimonials"].map(
         (item, index) => (
           <a
             href={`#${item}`}
