b2c21c6193d6285c62de1f093268af1326ab26ad
Alvin Kariuki <alvinkariuki230@gmail.com>
2023-04-12 13:49:01 +0300

    folder refactor changes

--- frontend_react/src/constants/images.jsx
+++ frontend_react/src/constants/images.jsx
@@ -1,9 +1,7 @@
 import email from "../assets/email.png";
 import mobile from "../assets/mobile.png";
-import api from "../assets/api.png";
 import cpp from "../assets/cpp.png";
 import css from "../assets/css.png";
-import figma from "../assets/figma.png";
 import flutter from "../assets/flutter.png";
 import git from "../assets/git.png";
 import graphql from "../assets/graphql.png";
@@ -18,20 +16,10 @@ import sass from "../assets/sass.png";
 import typescript from "../assets/typescript.png";
 import vue from "../assets/vue.png";
 
-import about01 from "../assets/about01.png";
-import about02 from "../assets/about02.png";
-import about03 from "../assets/about03.png";
-import about04 from "../assets/about04.png";
-
 import profile from "../assets/profile.png";
 import circle from "../assets/circle.svg";
-import logo from "../assets/logo.png";
+import logo from "../assets/My-project-1.png";
 
-import adidas from "../assets/adidas.png";
-import amazon from "../assets/amazon.png";
-import asus from "../assets/asus.png";
-import bolt from "../assets/bolt.png";
-import nb from "../assets/nb.png";
 import skype from "../assets/skype.png";
 import spotify from "../assets/spotify.png";
 import meProfile from "../assets/newimg.png";
@@ -42,10 +30,8 @@ import yinyang from "../assets/yin-yang.jpg";
 export default {
   email,
   mobile,
-  api,
   cpp,
   css,
-  figma,
   flutter,
   git,
   graphql,
@@ -59,18 +45,9 @@ export default {
   sass,
   typescript,
   vue,
-  about01,
-  about02,
-  about03,
-  about04,
   profile,
   circle,
   logo,
-  adidas,
-  amazon,
-  asus,
-  bolt,
-  nb,
   skype,
   spotify,
   meProfile,
